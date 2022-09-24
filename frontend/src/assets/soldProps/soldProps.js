017f99bfac4d7e6cf494caa1775cc82ae2fe28e2
Noud63 <noudvandun@gmail.com>
2022-09-24 12:14:51 +0200

    show more

--- frontend/src/assets/soldProps/soldProps.js
+++ frontend/src/assets/soldProps/soldProps.js
@@ -1,77 +1,55 @@
 const soldProps = [
     {
         id: 0,
-        name:'Chateau de Tourreau',
-        price: '5.890.000',
-        img: '../soldProps/fulfillyourdreamimg2.jpg'
+        line1: `Sold properties in this section,`,
+        line2: `are displayed with the express consent`,
+        line3: `of the owner of the property.`,
+        img: '../soldProps/message.jpg',
+        name: 'Ellie Castelli',
+        price: '@elliecastelli'
     },
     {
         id: 1,
         name: 'Chateau Les Carrasses',
-        price: '3.750.000',
+        price: '$3.750.000,-',
         img: '../soldProps/fulfillyourdreamimg3.jpg'
     },
     {
         id: 2,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
     },
     {
         id: 3,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
     },
     {
         id: 4,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
     },
     {
         id: 5,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
     },
     {
         id: 6,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
     },
     {
         id: 7,
         name: 'Chateau de Tourreau',
-        price: '5.890.000',
+        price: '$5.890.000,-',
         img: '../soldProps/fulfillyourdreamimg2.jpg'
-    },
-    {
-        id: 8,
-        name: 'Chateau de Tourreau',
-        price: '5.890.000',
-        img: '../soldProps/fulfillyourdreamimg2.jpg'
-    },
-    {
-        id: 9,
-        name: 'Chateau de Tourreau',
-        price: '5.890.000',
-        img: '../soldProps/fulfillyourdreamimg2.jpg'
-    },
-    {
-        id: 10,
-        name: 'Chateau de Tourreau',
-        price: '5.890.000',
-        img: '../soldProps/fulfillyourdreamimg2.jpg'
-    },
-    {
-        id: 11,
-        name: 'Chateau de Tourreau',
-        price: '5.890.000',
-        img: '../soldProps/fulfillyourdreamimg2.jpg'
-    },
-    
+    }
 ]
 
 export default soldProps
--- frontend/src/components/SoldProperties.js
+++ frontend/src/components/SoldProperties.js
@@ -1,33 +1,30 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import '../sassStyles/layout/SoldProperties.scss'
 import soldProps from '../assets/soldProps/soldProps'
 
 const SoldProperties = () => {
 
-    const [ soldProperties, setSoldProperties ] = useState([])
-    const [ loadBtn, setLoadBtn ] = useState('Show More')
-    const [ currentSlice, setCurrentSlice ] = useState(4)
-    const [ perPage, setPerPage ] = useState(4)
+    const [soldProperties, setSoldProperties] = useState([])
+    const [loadBtn, setLoadBtn] = useState('Show More')
+    const [currentSlice, setCurrentSlice] = useState(4)
+    const [perPage, setPerPage] = useState(4)
 
-    useEffect(()=> {
-       setSoldProperties(soldProps)
+    useEffect(() => {
+        setSoldProperties(soldProps)
     }, [])
 
     const loadMore = () => {
-       
-           
-        if (currentSlice < soldProperties.length ) {
+        if (currentSlice < soldProperties.length) {
             setCurrentSlice(currentSlice + perPage)
         }
 
-        if (currentSlice === (soldProperties.length - perPage)){
+        if (currentSlice === (soldProperties.length - perPage)) {
             setLoadBtn('Show Less')
         }
-        if (currentSlice === soldProperties.length){
-           setCurrentSlice(perPage)
-           setLoadBtn('Show More')
-       }
-        
+        if (currentSlice === soldProperties.length) {
+            setCurrentSlice(perPage)
+            setLoadBtn('Show More')
+        }
     }
 
     return (
@@ -35,10 +32,13 @@ const SoldProperties = () => {
             <div className="soldprops_header">Sold Properties:</div>
             <div className="soldprops_grid">
                 {soldProperties && soldProperties.slice(0, currentSlice).map(property => {
-                    const { img, name, price, id } = property
+                    const { img, name, price, id, line1, line2, line3 } = property
                     return <div className="soldprops_grid_imageBox" key={id}>
+                        {line1 && line2 && line3 ? <div className="message">
+                            <div className="message_text"><span>{line1}</span><span>{line2}</span><span>{line3}</span><span style={{fontSize: '10px'}}>{price}</span></div>
+                           </div> : "" }
                         <div className="image"><img src={process.env.PUBLIC_URL + `/soldProps/${img}`} alt="" style={{ width: '100%', height: 'auto' }} /></div>
-                        <div className="footer"><span className="soldChateau">{name}</span><span className="soldPrice">${price},-</span></div>
+                        <div className="footer"><span className="soldChateau">{name}</span><span className="soldPrice">{price}</span></div>
                     </div>
                 })}
             </div>
