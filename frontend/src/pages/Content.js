9431a1dcf2667d24266fb3f7449d4b2e0a8da54b
Noud63 <noudvandun@gmail.com>
2022-09-25 13:28:31 +0200

    sidebar

--- frontend/src/pages/Content.js
+++ frontend/src/pages/Content.js
@@ -13,6 +13,8 @@ import frenchproperty from '../assets/images/goodlife.jpg'
 import magazine2 from '../assets/images/magazine2.jpg'
 import magazine3 from '../assets/images/magazine4.png'
 import SoldProperties from '../components/SoldProperties'
+import 'react-tippy/dist/tippy.css'
+import { Tooltip } from 'react-tippy'
 
 
 const Content = () => {
@@ -126,23 +128,29 @@ const Content = () => {
                             </div>
 
                             <div className="imageBox" >
+                                <Tooltip title="The Good Life magazine" position="top" trigger="mouseenter" inertia="true" theme='light'>
                                 <a href="https://thegoodlifefrance.com/category/magazine/" target="blank">
                                     <div className="sidebarimg" style={{ opacity: '1' }}><img src={frenchproperty} alt="" /></div>
                                 <div className="sidebartext"><span className="soldChateau">Subscribe now!</span><span className="soldPrice">It's free!</span></div></a>
+                                </Tooltip>
                             </div>
 
                             <div className="imageBox" >
+                                <Tooltip title="French Property News Magazine" position="top" trigger="mouseenter" inertia="true" theme='light' >
                                 <a href="https://www.afrenchcollection.com/french-property-news-magazine/" target="blank">
                                     <div className="sidebarimg" style={{ opacity: '1' }}><img src={magazine2} alt="" /></div>
                                     <div className="sidebartext"><span className="soldChateau">Subscribe now!</span><span className="soldPrice">$3.99 p/m</span></div></a>
+                                </Tooltip>
                             </div>
-
+                           
                             <div className="imageBox" >
+                                <Tooltip title="My French Country Home Magazine" position="top" trigger="mouseenter" inertia="true" theme='light'>
                                 <a href="https://myfrenchcountryhomemagazine.com/product-category/digital-edition/" target="blank">
                                     <div className="sidebarimg" style={{ opacity: '1' }}><img src={magazine3} alt="" /></div>
                                     <div className="sidebartext"><span className="soldChateau">Subscribe now!</span><span className="soldPrice">$39.99 p/y</span></div></a>
+                                </Tooltip>
                             </div>
-
+                           
                         </div>
                     </div>
                 </div>
