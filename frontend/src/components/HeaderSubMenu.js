a1352b2c24956c358659467db516abafaca2aac9
Noud63 <noudvandun@gmail.com>
2023-02-17 13:38:08 +0100

    menu

--- frontend/src/components/HeaderSubMenu.js
+++ frontend/src/components/HeaderSubMenu.js
@@ -3,7 +3,7 @@ import React from 'react'
 const HeaderSubMenu = ({ showMenu, buy, scrolled, showUserInfo, register, loginHandler, isLoggedIn, logOut, size}) => {
 
     return (
-        <div className={showMenu && size.width < 800 && !scrolled ? "menuOverlay show" : scrolled ? "menuOverlay show toTop" : "menuOverlay"}>
+        <div className={showMenu && size.width < 800 && !scrolled ? "menuOverlay show" : size.width < 800 && scrolled ? "menuOverlay show toTop" : "menuOverlay"}>
             <button className="buttons_btnOverlay" onClick={buy}>Buy</button>
             <button className="buttons_btnOverlay" onClick={loginHandler}>{!logOut ? 'Login' : 'Logout'}</button>
             {isLoggedIn && <button className="buttons_btnOverlay" onClick={showUserInfo}>user info</button>}
