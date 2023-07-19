f471d6869fc488c2b5c75a28707ef916f72de505
Noud63 <noudvandun@gmail.com>
2023-07-19 11:49:18 +0200

    cookie

--- frontend/src/components/InfoModal.js
+++ frontend/src/components/InfoModal.js
@@ -8,19 +8,20 @@ const InfoModal = ({ showPopup, item, closeModal}) => {
     return (
         <div className={showPopup ? "estateInfoPopup" : "estateInfoPopup hide"} onClick={closeModal}>
               <div className="estateInfoPopup_estateInfoBox">
-                    <div className="estateInfoPopup_estateInfoBox_header"><h3>{name}</h3></div>
-                <div className="estateInfoPopup_estateInfoBox_gridContainer" >
-                   {img.map( (img, index) => {
-                       return (
-                        <div key={index}>
-                        <div className="estateImage" key={index}><img src={process.env.PUBLIC_URL + `assets/images/${img}`} alt="estate" /></div>
-                        <div className="estateInfoPopup_moreinfo">More info on: <b>{name}</b>&nbsp;coming soon!</div>
-                        </div>
-                           
+                    <div className="estateInfoPopup_estateInfoBox_header">
+                        <h3>{name}</h3>
+                    </div>
+                 <div className="estateInfoPopup_estateInfoBox_gridContainer" >
+                     {img.map( (img, index) => {
+                        return (
+                         <div key={index}>
+                             <div className="estateImage" key={index}><img src={process.env.PUBLIC_URL + `assets/images/${img}`} alt="estate" /></div>
+                             <div className="estateInfoPopup_moreinfo">More info on: <b>{name}</b>&nbsp;coming soon!</div>
+                         </div>
                        )
-                   })}
-                   </div>
-                </div>
+                      })}
+                 </div>
+              </div>
         </div>
     )
 }
